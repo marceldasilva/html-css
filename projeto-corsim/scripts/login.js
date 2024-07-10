a6f1f7d26bec3c92f8bbc66a905830050c10b024
Marcel Alves da Silva da Costa <122131624+marceldasilva@users.noreply.github.com>
2024-07-09 23:31:19 -0300

    andando mais com o projeto

--- projeto-corsim/scripts/login.js
+++ projeto-corsim/scripts/login.js
@@ -1,3 +1,16 @@
+const email = document.querySelector('#iemail')
+const senha = document.querySelector('#isenha')
+
+function cadastraUsuario(emailUsuario, senhaUsuario) {
+    const Usuario = {
+        email: emailUsuario,
+        senha: senhaUsuario
+    }
+    return Usuario
+}
+
+console.log(cadastraUsuario(email, senha))
+
 function logar() {
-    alert('FUI CLICADO!')
+    validaLogin()
 }
